fa4b0bb48578e27c10c97f64d537bf6484c52831
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-02-27 23:44:42 +0200

    this was classwork

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,8 +15,17 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let allPTags = document.querySelectorAll("p");
+console.log("p", allPTags.length);
 
+let divElement = document.querySelector('div');
+console.log("div", divElement);
 
+let elementId = document.getElementById('jumbotron-text');
+console.log("jumbotron", elementId);
+
+let insidePrimary = document.querySelector('.primary-content').querySelectorAll('p');//css way of chaining 'primary-content p'
+console.log('insidePrimary', insidePrimary);
 /*
 Task 2
 ======
@@ -24,6 +33,15 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+// let findButton = document.querySelector('#alertBtn');   //Ryno's way
+let findButton = document.getElementById('alertBtn');   //Douglas' way
+
+console.log('findButton', findButton);
+findButton.addEventListener('click', doSomething);
+
+function doSomething() {
+    alert('Thank for visiting the Bikes for Refugees!');    
+}
 
 /*
 Task 3
@@ -39,6 +57,15 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+let addSomeButton = document.getElementById('addTextBtn' );
+
+console.log('addSomeButton', addSomeButton);
+addSomeButton.
+// findButton.addEventListener('click', doSomething);
+
+// function doSomething() {
+//     alert('Thank for visiting the Bikes for Refugees!');    
+// }
 
 
 
